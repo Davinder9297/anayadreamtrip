83a95a06ee7b7106e6f6600450dc85d3655f5491
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2025-01-17 11:18:31 +0530

    bug

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+import HeroSection from "@/pages/Homepage/HeroSection";
+
+const Header = () => {
+  return (
+      <div className="mt-0"><HeroSection /></div>
+
+  );
+};
+
+export default Header;
