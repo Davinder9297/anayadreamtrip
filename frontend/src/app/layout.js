b4aac96bc7aaeb00d01c117b06ad453c9f165756
Davinder9297 <davindergiri765@gmail.com>
2025-01-19 22:09:35 +0530

    resolved the import error and apply pointer on header

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,7 @@
 // src/app/layout.js
-import Header from "@/components/Navbar/Navbar.jsx";
+import Header from "../components/Navbar/Navbar";
 import "./globals.css";
-import Footer from "@/components/Footer/Footer.jsx";
+import Footer from "../components/Footer/Footer";
 
 export const metadata = {
   title: "Anaya Dream Trip",
