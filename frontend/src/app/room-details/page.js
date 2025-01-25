3da2987ed3ca4a8f41b84660add4f9eb3dc90554
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2025-01-25 10:26:07 +0530

    conflicts merged

--- frontend/src/app/room-details/page.js
+++ frontend/src/app/room-details/page.js
@@ -5,12 +5,7 @@ const Page = () => {
     return (
         <>
             {/* <Router> */}
-<<<<<<< HEAD
-            {/* <div className=""><Header/></div> */}
             <div className="flex"><RoomDetails/></div>
-=======
-            <div className="pt-20 flex"><RoomDetails/></div>
->>>>>>> 2f82a6e07df8d0f8a5a58727d2853165c010c6f7
             {/* </Router> */}
         </>
     );
